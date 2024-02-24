48d898a3dcfdacf7b9fe4c82888498544eb47110
Louis Cavrenne <124626955+louisc-un@users.noreply.github.com>
2024-02-24 15:15:47 +0100

    IMG-7: Initialize Vue.js env (#1)
    * init project
    
    * setup vue project
    
    * add flowbite to the project

---
+++ GUI/src/components/Main.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ModalExample from '@/components/ModalExample.vue';
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+
+  <ModalExample />
+
+</template>
+
