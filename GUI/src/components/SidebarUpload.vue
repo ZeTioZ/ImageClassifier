5c1c59e3d45803971cbadb86ce95346654777965
Louis Cavrenne <124626955+louisc-un@users.noreply.github.com>
2024-02-28 17:27:30 +0100

    IMG-10: faire le module d'upload (#5)
    * add sidebar comp for upload module
    
    * add sidebar, dropzone and archive cards
    
    * move file card in its own component
    
    * improve archive card style

---
+++ GUI/src/components/SidebarUpload.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+import ArchiveDropzone from '@/components/ArchiveDropzone.vue';
+import ArchiveCard from '@/components/ArchiveCard.vue';
+
+const fileData = ref([
+  {
+    fileName: "archive3.zip",
+    fileSize: "95MB",
+    nbImages: "97"
+  },
+  {
+    fileName: "camp57A.zip",
+    fileSize: "1.65GB",
+    nbImages: "256"
+  },
+  {
+    fileName: "20230605.zip",
+    fileSize: "1.05GB",
+    nbImages: "188"
+  }
+])
+</script>
+
+<template>
+  <aside id="upload-sidebar" class="z-40 w-64 h-full transition-transform -translate-x-full translate-x-0" aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-100 border-e border-black flex flex-col space-y-3">
+
+      <ArchiveDropzone /> 
+
+      <span v-for="file in fileData">
+        <ArchiveCard :fileName="file.fileName" :fileSize="file.fileSize" :nbImages="file.nbImages" />
+      </span>
+
+    </div>
+  </aside>
+</template>
