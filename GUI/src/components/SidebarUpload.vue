fd6289b8c5d53122953607b406161fb07760ffef
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-02-28 11:45:39 +0100

    add sidebar comp for upload module

---
+++ GUI/src/components/SidebarUpload.vue
@@ -0,0 +1,7 @@
+<template>
+  <aside id="separator-sidebar" class="z-40 w-64 h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-100 border-e border-black">
+
+    </div>
+  </aside>
+</template>
