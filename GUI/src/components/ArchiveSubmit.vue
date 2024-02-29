25b46432d252db283964e93b061c21cae1589ab6
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-02-29 21:59:51 +0100

    add new tag input when submitting archive

--- GUI/src/components/ArchiveSubmit.vue
+++ GUI/src/components/ArchiveSubmit.vue
@@ -1,8 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+import ArchiveSubmitNewTag from '@/components/ArchiveSubmitNewTag.vue';
+
+const newTag = ref('');
+const newTags = ref([]);
+
+const addNewTag = () => {
+  if (newTag.value) {
+    newTags.value.push(newTag.value);
+    newTag.value = '';
+  }
+};
+</script>
+
+
 <template>
   <div class="p-3">
     <!-- tags -->
     <div>
+      <form v-on:submit.prevent="addNewTag">
+        <label for="new-tag">Ajouter un mot-clé</label>
+        <input v-model="newTag" id="new-tag" placeholder="Ex.: voiture, piscine, etc." />
+        <button>+</button>
+      </form>
+      <ul>
+        <ArchiveSubmitNewTag v-for="tag in newTags" :tagName="tag" @remove="" />
+      </ul>
     </div>
+
     <!-- submit button-->
     <div>
       <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 focus:outline-none w-full">Trier images</button>
---
+++ GUI/src/components/ArchiveSubmitNewTag.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['tagName']);
+const emits = defineEmits(['remove']);
+</script>
+
+<template>
+  <li>
+    {{ tagName }}
+    <button @click="$emit('remove')">Remove</button>
+  </li>
+</template>
