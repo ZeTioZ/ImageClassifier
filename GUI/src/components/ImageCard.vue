f244a8260d7082314167641690970e666e088a5f
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-02-29 16:44:37 +0100

    add checkbox to image component

--- GUI/src/components/ImageCard.vue
+++ GUI/src/components/ImageCard.vue
@@ -12,13 +12,17 @@ const props = defineProps(['imgSrc', 'index', 'fileName', 'tags']);
       <img :src="imgSrc" :alt="`Workspace Image ${index + 1}`" class="h-full w-full object-cover">
     </div>
     <!-- Three dots/menu icon -->
-    <a href="#" class="absolute top-0 right-0">
+    <a href="#" class="absolute top-0 left-0">
       <div class="m-1 rounded-full bg-gray-100">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
         </svg>
       </div>
     </a>
+    <!-- Checkbox for selection -->
+    <div class="absolute top-0 right-0 flex items-center m-2">
+      <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    </div>
     <!-- Tag occupying the remaining 20% -->
     <div class="h-1/3 w-full flex flex-col justify-center p-1">
       <span class="text-sm font-bold text-center">{{ fileName }}</span>
