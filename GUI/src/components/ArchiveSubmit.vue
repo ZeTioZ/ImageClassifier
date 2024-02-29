bafce51b334ffee0c66ec2256cf1ecbbca8b5c6a
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-02-29 17:15:49 +0100

    add submit button

---
+++ GUI/src/components/ArchiveSubmit.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-3">
+    <!-- tags -->
+    <div>
+    </div>
+    <!-- submit button-->
+    <div>
+      <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 focus:outline-none w-full">Trier images</button>
+    </div>
+  </div>
+</template>
