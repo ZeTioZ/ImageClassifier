3c861945314dc2a9fa045f84197c50cf579d61cf
ZeTioZ <donato_gentile@hotmail.it>
2024-02-29 17:33:33 +0100

    Changing model and fixed files paths
    Co-authored-by: Djingoo <Djingoo@users.noreply.github.com>
    
    Took 1 hour 54 minutes

---
+++ AI/src/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+from modules.path_utils import get_parent_path
+
+root_path = get_parent_path(__file__, 2)
+join_char = os.path.sep
+extracted_path = f'{root_path}{join_char}extracted{join_char}'
+model_path = f'{root_path}{join_char}models{join_char}yolov8x-oiv7.pt'
+supported_formats = [".bmp", ".dib", ".jpeg", ".jpg", ".jpe", ".jp2", ".png", ".webp", ".avif", ".pbm", ".pgm", ".ppm",
+                     ".pxm", ".pnm", ".pfm", ".sr", ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic"]
--- AI/src/modules/zip.py
+++ AI/src/modules/zip.py
@@ -1,6 +1,8 @@
 import zipfile
 import pathlib
 
+from AI.src import supported_formats, extracted_path
+
 
 def extract_zip(zips_path, file_batch_name: str = None) -> bool:
 	for zip_path in zips_path:
@@ -8,7 +10,7 @@ def extract_zip(zips_path, file_batch_name: str = None) -> bool:
 			continue
 		if zipfile.is_zipfile(zip_path):
 			with zipfile.ZipFile(zip_path, 'r', allowZip64=True) as zip_ref:
-				zip_ref.extractall("../extracted/")
+				zip_ref.extractall(extracted_path)
 		else:
 			print(f"{zip_path} is not a valid zip file.")
 	if file_batch_name is not None:
@@ -16,6 +18,9 @@ def extract_zip(zips_path, file_batch_name: str = None) -> bool:
 	return True
 
 
-def rename_extracted_files(new_name: str = None, folder_path="../extracted/"):
+def rename_extracted_files(new_name: str = None, folder_path=extracted_path):
 	for index, file in enumerate(pathlib.Path(folder_path).iterdir()):
-		file.rename(pathlib.Path(folder_path) / f"{new_name}_{index}{file.suffix}")
+		if file.suffix not in supported_formats:
+			continue
+		indexed_name = f"{new_name} #{index}" if len(new_name) > 0 else f"#{index}"
+		file.rename(pathlib.Path(folder_path) / f"{indexed_name}{file.suffix}")
