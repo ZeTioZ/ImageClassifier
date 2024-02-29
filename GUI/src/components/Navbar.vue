917cde732053752a41c959244e39ad2cd416e681
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-01 00:58:48 +0100

    add save button

--- GUI/src/components/Navbar.vue
+++ GUI/src/components/Navbar.vue
@@ -8,16 +8,21 @@
       <div class="block w-auto" id="navbar-solid-bg">
         <ul class="flex flex-row font-medium rounded-lg space-x-8 mt-0 border-0 bg-transparent">
           <li>
-            <a href="#" class="block py-2 p-0 text-gray-900 rounded hover:bg-transparent border-0 hover:text-blue-700">Action1</a>
+            <a href="#" class="block py-2 p-0 text-gray-900 rounded border-0">Action1</a>
           </li>
           <li>
-            <a href="#" class="block py-2 p-0 text-gray-900 rounded hover:bg-transparent border-0 hover:text-blue-700">Action2</a>
+            <a href="#" class="block py-2 p-0 text-gray-900 rounded border-0">Action2</a>
           </li>
           <li>
-            <a href="#" class="block py-2 p-0 text-gray-900 rounded hover:bg-transparent border-0 hover:text-blue-700">Action3</a>
+            <a href="#" class="block py-2 p-0 text-gray-900 rounded border-0">Action3</a>
           </li>
           <li>
-            <a href="#" class="block py-2 p-0 text-gray-900 rounded hover:bg-transparent border-0 hover:text-blue-700">Action4</a>
+            <a href="#" class="block py-2 px-3 p-0 text-white rounded-lg border-0 bg-gray-700 flex flex-row items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+              </svg>
+              <span class="ms-1">Sauvegarder</span>
+            </a>
           </li>
         </ul>
       </div>
