1dbde899c110facd8c18e1b033d6b0e3c8660330
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-06 18:11:21 +0100

    create icons folder

--- GUI/src/components/ArchiveCard.vue
+++ GUI/src/components/ArchiveCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Tag from '@/components/icons/Tag.vue';
+
 const props = defineProps(['fileName', 'fileSize', 'nbImages']); 
 </script>
 
@@ -6,9 +8,7 @@ const props = defineProps(['fileName', 'fileSize', 'nbImages']);
   <div class="relative">
     <a href="#" class="absolute top-0 right-0">
       <div class="p-1 m-1 rounded-full hover:bg-gray-100">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
+        <Tag class="w-6 h-6"/>
       </div>
     </a>
 
