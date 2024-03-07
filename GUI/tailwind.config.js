af6e6b624333902e84b394acb1304b032a5602e3
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-07 16:26:00 +0100

    add config in tailwind.config.js

--- GUI/tailwind.config.js
+++ GUI/tailwind.config.js
@@ -6,7 +6,27 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ls-vert-base": "var(--ls-vert-base)",
+        "ls-vert-fonce": "var(--ls-vert-fonce)",
+        "ls-bleu-fonce": "var(--ls-bleu-fonce)",
+
+        "ls-prune": "var(--ls-prune)",
+        "ls-orange": "var(--ls-orange)",
+        "ls-gris": "var(--ls-gris)",
+        "ls-turquoise": "var(--ls-turquoise)",
+        "ls-bleu-clair": "var(--ls-bleu-clair)",
+        "ls-rouge": "var(--ls-rouge)",
+        "ls-rose": "var(--ls-rose)",
+
+        "ls-baladins": "var(--ls-baladins)",
+        "ls-louveteaux": "var(--ls-louveteaux)",
+        "ls-eclaireurs": "var(--ls-eclaireurs)",
+        "ls-pionniers": "var(--ls-pionniers)",
+        "ls-mondial": "var(--ls-mondial)",
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
