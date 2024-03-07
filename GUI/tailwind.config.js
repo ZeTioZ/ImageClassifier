51a6c613e5ca02d8ed25dc46ca1cdf6dd0b9e41e
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-07 16:40:22 +0100

    update font family in tailwind config

--- GUI/tailwind.config.js
+++ GUI/tailwind.config.js
@@ -6,27 +6,28 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {
-      colors: {
-        "ls-vert-base": "var(--ls-vert-base)",
-        "ls-vert-fonce": "var(--ls-vert-fonce)",
-        "ls-bleu-fonce": "var(--ls-bleu-fonce)",
+    colors: {
+      "ls-vert-base": "var(--ls-vert-base)",
+      "ls-vert-fonce": "var(--ls-vert-fonce)",
+      "ls-bleu-fonce": "var(--ls-bleu-fonce)",
 
-        "ls-prune": "var(--ls-prune)",
-        "ls-orange": "var(--ls-orange)",
-        "ls-gris": "var(--ls-gris)",
-        "ls-turquoise": "var(--ls-turquoise)",
-        "ls-bleu-clair": "var(--ls-bleu-clair)",
-        "ls-rouge": "var(--ls-rouge)",
-        "ls-rose": "var(--ls-rose)",
+      "ls-prune": "var(--ls-prune)",
+      "ls-orange": "var(--ls-orange)",
+      "ls-gris": "var(--ls-gris)",
+      "ls-turquoise": "var(--ls-turquoise)",
+      "ls-bleu-clair": "var(--ls-bleu-clair)",
+      "ls-rouge": "var(--ls-rouge)",
+      "ls-rose": "var(--ls-rose)",
 
-        "ls-baladins": "var(--ls-baladins)",
-        "ls-louveteaux": "var(--ls-louveteaux)",
-        "ls-eclaireurs": "var(--ls-eclaireurs)",
-        "ls-pionniers": "var(--ls-pionniers)",
-        "ls-mondial": "var(--ls-mondial)",
-      }
+      "ls-baladins": "var(--ls-baladins)",
+      "ls-louveteaux": "var(--ls-louveteaux)",
+      "ls-eclaireurs": "var(--ls-eclaireurs)",
+      "ls-pionniers": "var(--ls-pionniers)",
+      "ls-mondial": "var(--ls-mondial)",
     },
+    fontFamily: {
+      sans: ['Muli', 'sans-serif'],
+    }
   },
   plugins: [
     require('flowbite/plugin')
