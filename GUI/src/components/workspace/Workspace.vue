fcd5c12805ab1d7beee5ca778f954a2b94a5d59b
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-03-11 10:35:17 +0100

    draggableV1
    premiere version du drag and drop, a impémenter entre les deu colones

--- GUI/src/components/workspace/Workspace.vue
+++ GUI/src/components/workspace/Workspace.vue
@@ -1,7 +1,11 @@
 <script setup>
 import ImageCard from '@/components/image/ImageCard.vue';
+import draggable from 'vuedraggable';
+import { ref } from 'vue';
 
 const props = defineProps(['workspaceName', 'images']);
+
+const draggableImages = ref(props.images);
 </script>
 
 <template>
@@ -14,14 +18,22 @@ const props = defineProps(['workspaceName', 'images']);
     <!-- Workspace where images are managed -->
     <div class="flex-1 border-gray-500 border-2 rounded-lg bg-gray-100 p-2 overflow-auto">
       <!-- Images Display -->
-      <div class="grid grid-cols-3 gap-4">
-        <div v-for="(image, index) in props.images" :key="index" class="flex flex-col items-center">
-          <ImageCard :imgSrc="image" :index="index" fileName="filename.png" :tags="['scout', 'enfant']" />  
-        </div>
-      </div>
+      <draggable class="grid grid-cols-3 gap-4" v-model="draggableImages" item-key="index" @end="onEnd">
+        <template #item="{ element, index }">
+          <div class="flex flex-col items-center">
+            <ImageCard :imgSrc="element" :index="index" fileName="filename.png" :tags="['scout', 'enfant']"/>
+          </div>
+        </template>
+      </draggable>
     </div>
   </div>
 </template>
+
+<script>
+function onEnd(event) {
+  console.log('Élément déplacé:', event);
+}
+</script>
 
 <style scoped>
 /* Adjust styles if necessary */
