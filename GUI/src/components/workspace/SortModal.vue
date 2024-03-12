c4c7f365cd625b593054f6eb17502dcd9b129ac8
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-03-12 17:26:32 +0100

    color fix

--- GUI/src/components/workspace/SortModal.vue
+++ GUI/src/components/workspace/SortModal.vue
@@ -7,7 +7,7 @@
             <input v-model="newTag" id="new-tag" placeholder="Ex.: voiture, piscine, etc." autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-l-md block p-1 w-full" />
           </div>
           <div class="items-center px-4 py-3">
-            <button id="ok-btn" @click="closeModal" class="px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300">
+            <button id="ok-btn" @click="closeModal" class="px-4 py-2 bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 text-white text-base font-medium rounded-md w-full shadow-sm ">
               Chercher
             </button>
           </div>
