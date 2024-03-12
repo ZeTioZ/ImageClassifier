05fe4de2b4bbf0ba41f4cf84aa0ac4a8a2d54d7a
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-03-12 17:00:02 +0100

    Update Workspace.vue

--- GUI/src/components/workspace/Workspace.vue
+++ GUI/src/components/workspace/Workspace.vue
@@ -8,15 +8,34 @@ const props = defineProps({
   images: Array
 });
 
-// Référence réactive pour les images (en supposant que vous souhaitiez permettre la réactivité)
+// Référence réactive pour les images 
 const draggableImages = ref(props.images);
+
+function onEnd(event) {
+  // Vérifie si l'élément a été déplacé vers une liste différente
+  if (event.from !== event.to) {
+    // TO DO modifier les information de l'image 
+    console.log('L\'élément a été déplacé vers une autre liste.');
+
+    // Affiche les indices de départ et d'arrivée
+    console.log('Indice de départ:', event.oldIndex);
+    console.log('Nouvel indice:', event.newIndex);
+
+    console.log(draggableImages.value)
+
+    } else {
+    console.log('L\'élément a été réorganisé dans la même liste.');
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-col text-center p-3 h-full">
+    <!-- Title -->
     <div class="p-5">
       <h2 class="text-2xl font-bold">{{ props.workspaceName }}</h2>
     </div>
+    <!-- Workspace where images are managed -->
     <div class="flex-1 border-gray-500 border-2 rounded-lg bg-gray-100 p-2 overflow-auto">
       <draggable class="min-h-[200px] grid grid-cols-3 gap-4" group="images" v-model="draggableImages" item-key="index" @end="onEnd">
         <template #item="{ element, index }">
