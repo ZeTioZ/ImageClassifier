81921c0d0ae5bc63614020cb1cb476301696dfdb
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-13 22:33:19 +0100

    change archive card style

--- GUI/src/components/archive/ArchiveCard.vue
+++ GUI/src/components/archive/ArchiveCard.vue
@@ -7,12 +7,12 @@ const props = defineProps(['fileName', 'fileSize', 'nbImages']);
 <template>
   <div class="relative">
     <a href="#" class="absolute top-0 right-0">
-      <div class="p-1 m-1 rounded-full hover:text-ls-bleu-fonce hover:bg-gray-100">
+      <div class="p-1 m-1 rounded-full text-ls-bleu-fonce hover:bg-gray-100">
         <HEllipsis class="w-6 h-6"/>
       </div>
     </a>
 
-    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded">
+    <div class="block max-w-sm p-6 bg-white rounded">
       <h5 class="mb-2 text-lg font-semibold tracking-tight text-ls-bleu-fonce">{{ fileName }}</h5>
       <p class="text-sm font-normal text-gray-700"><strong>{{ nbImages }} images</strong> ({{ fileSize }})</p>
     </div>
--- GUI/src/components/archive/ArchiveDropzone.vue
+++ GUI/src/components/archive/ArchiveDropzone.vue
@@ -4,10 +4,11 @@ import Cloud from '@/components/icons/Cloud.vue';
 
 <template>
   <div class="flex items-center justify-center w-full">
-    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-42 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-42 border-2 border-ls-bleu-fonce border-dashed rounded cursor-pointer bg-white">
       <div class="flex flex-col items-center justify-center pt-5 pb-6">
-        <Cloud class="w-8 h-8 mb-4 text-gray-700" /> 
-        <p class="mb-2 text-sm text-gray-900 text-center"><span class="font-semibold">Cliquer pour uploader</span><br> ou glisser et déposer</p>            <p class="text-xs text-gray-500">.ZIP</p>
+        <Cloud class="w-8 h-8 mb-4 text-ls-bleu-fonce" /> 
+        <p class="mb-2 text-sm text-ls-bleu-fonce text-center"><strong>Cliquer pour uploader</strong><br>(ou glisser et déposer)</p>
+        <p class="text-xs font-bold text-gray-700">.zip</p>
       </div>
       <input id="dropzone-file" type="file" class="hidden" />
     </label>
