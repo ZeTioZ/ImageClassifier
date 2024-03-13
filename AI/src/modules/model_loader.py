9bb37b551e3464b2ff724db0efb559ff45dbe17a
Djingoo <56677342+Djingoo@users.noreply.github.com>
2024-03-13 19:24:57 +0100

    added blur filter

--- AI/src/modules/model_loader.py
+++ AI/src/modules/model_loader.py
@@ -5,6 +5,7 @@ import pathlib
 from ultralytics import YOLO
 
 from AI.src import extracted_path, supported_formats
+from AI.src.modules import quality_check
 from .draw_utils import box_visualizer
 from .yolov8_converter_utils import convert_image_box_outputs
 
@@ -21,7 +22,7 @@ class ModelLoader:
 		return self.predictions
 
 
-def generate_tags(model_path: str, default_tags: list[str] = None) -> dict[str, list[str]]:
+def generate_tags(model_path: str, default_tags: list[str] = None) -> dict[str, dict[str, bool | list[str]]]:
 	"""
 	Generates tags for the given images using the given model.
 
@@ -36,16 +37,20 @@ def generate_tags(model_path: str, default_tags: list[str] = None) -> dict[str, 
 		if file.is_file() and file.suffix in supported_formats:
 			image_path = file.absolute().__str__()
 			image = cv2.imread(image_path)
+			is_qualitative, quality_tags = quality_check.is_qualitative(image)
 			predictions = model.predict(image)
 			boxes = convert_image_box_outputs(predictions)
 			# FOR DEMO
 			prediction_image = box_visualizer(image, boxes, print_classes=True)
 			multiplier = image.shape[1] / 1000
 			prediction_image = cv2.resize(prediction_image,
-			                              (int(image.shape[1] / multiplier), int(image.shape[0] / multiplier)))
+										 (int(image.shape[1] / multiplier), int(image.shape[0] / multiplier)))
 			cv2.imshow('Prediction', prediction_image)
 			cv2.waitKey(0)
 			cv2.destroyAllWindows()
 			# END FOR DEMO
-			tags[file.name] = list(dict.fromkeys([box.cls for box in boxes] + default_tags))
+			tags[file.name] = {"detection_tags": list(dict.fromkeys([box.cls for box in boxes])) +
+									   default_tags,
+							   "is_qualitative": is_qualitative,
+							   "quality_tags": list(filter(lambda x: quality_tags[x], quality_tags.keys()))}
 	return tags
