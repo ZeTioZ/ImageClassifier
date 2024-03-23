e6359d77b1df73b6f5d81da758c2eef4998481b4
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-03-23 18:49:13 +0100

    add jsdoc in image class

--- GUI/src/js/image.js
+++ GUI/src/js/image.js
@@ -21,30 +21,55 @@ export class Image {
     this.#toBeDeleted = false;
   }
 
+  /**
+  * get the entry of the image in the archive
+  *
+  * @return {import('@zip.js/zip.js').Entry} - the entry of the image 
+  */
   get archiveEntry() {
     return this.#archiveEntry;
   }
   
+  /**
+  * get a hash of the image, used for identification
+  *
+  * @return {string} - a hash 
+  */
   get hash() {
     return this.#hash;
   }
 
+  /**
+  * get the name of the image
+  *
+  * @return {string} - the name of the image
+  */
   get filename() {
     return this.filename;
   }
 
+  /**
+  * get the extension of the image
+  *
+  * @return {string} - the extension of the image
+  */
   get extension() {
     const re = /(?:\.([^.]+))?$/;
    
     return re.exec(this.#filename)[1];
   }
 
+  /**
+  * get the size of the image in bytes 
+  *
+  * @return {number} - the size of the image
+  */
   get size() {
     return this.#size;
   }
 
   /**
-  * get a human readable file size (in 'Octets'/bytes)
+  * get a human readable size of the file (in 'Octets'/bytes)
   * 
   * @return {string} - the size of the file in a human readable format
   */
@@ -60,19 +85,42 @@ export class Image {
     return `${size} ${unit}`;
   }
 
+  /**
+  * get the list of tags of the image
+  *
+  * @return {Array.<Tag>} - a list of tags
+  */
   get tags() {
     return this.#tags;
   }
 
+  /**
+  * tell if the image has the 'toBeDeleted' flag
+  *
+  * @return {boolean} - true if the image is to be deleted, false otherwise
+  */
   get toBeDeleted() {
     return this.#toBeDeleted;
   }
 
+  /**
+  * get the reason why the image should be deleted (i.e. the unmatched requirements)
+  *
+  * @return ? 
+  */
   get reasonForDeletion() {
     return this.#reasonForDeletion;
   }
 
+  /**
+  * get the url pointing to the image (i.e. src in img tag)
+  *
+  * @param {boolean} [thumbnail=true] - if a thumbnail or the full image should be sourced
+  * @returns {string} - the url of the image
+  */
   async getBlobURL(thumbnail = true) {
+    // create blob url if this.blobURL is undefined, create it otherwise 
+
     return "";
   }
 }
