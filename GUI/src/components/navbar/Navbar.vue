6b0260666c813e978c90a5cbcc9d27285ab10249
adebaets <anae.debaets@student.unamur.be>
2024-03-28 10:45:45 +0100

    Logo et Nom

--- GUI/src/components/navbar/Navbar.vue
+++ GUI/src/components/navbar/Navbar.vue
@@ -13,8 +13,8 @@ function toggleModalConfigIA(){
   <nav class="bg-ls-bleu-fonce">
     <div class="flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" class="flex items-center space-x-3 space-x-reverse">
-        <!-- <img src="" class="h-8" alt="" /> -->
-        <span class="self-center text-2xl font-bold whitespace-nowrap text-white">Image Classifier</span>
+        <img src="@/components/icons/Logo.png" class="h-12" alt="Scout Lens" />
+        <span class="self-center text-2xl font-bold whitespace-nowrap text-white">Scout Lens</span>
       </a>
       <div class="block w-auto" id="navbar-solid-bg">
         <ul class="flex flex-row font-medium rounded-lg space-x-8 mt-0 border-0 bg-transparent">
