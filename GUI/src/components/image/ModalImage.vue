9038acb83d2bf4b91151d76c8e4908fac20171ee
adebaets <anae.debaets@student.unamur.be>
2024-04-02 10:37:16 +0200

    Update ModalImage.vue

--- GUI/src/components/image/ModalImage.vue
+++ GUI/src/components/image/ModalImage.vue
@@ -1,19 +1,11 @@
 <script setup>
 import Tag from '@/components/image/Tag.vue'
 import {ref} from 'vue';
-const props = defineProps(['imgSrc', 'imgName', 'imgTags', 'imgSize']);
-const showAdd = ref(false);
-const showDel = ref(false);
+const props = defineProps(['imgSrc', 'imgName', 'imgTags', 'imgSize', 'imgSup']);
 const emit = defineEmits(['close', 'add', 'del']);
 
 function closeModal() {
   emit('close');
-}
-function toggleAdd(){
-  showAdd.value = !showAdd.value;
-}
-function toggleDel(){
-  showDel.value = !showDel.value;
 }
 function tagToAdd() {
   emit('add', addTag.value);
@@ -25,7 +17,7 @@ function tagToDel() {
 </script>
 <template>
 <!-- Extra Large Modal -->
-<div id="extralarge-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto bg-gray-600 bg-opacity-50 md:inset-0 h-[calc(100%-1rem)] max-h-full" @click.self="closeModal">
+<div id="extralarge-modal" tabindex="-1" class="fixed inset-0 top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto bg-gray-600 bg-opacity-50 md:inset-0 h-[calc(100%-1rem)] max-h-full" @click.self="closeModal">
     <div class="relative w-full max-w-7xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow">
@@ -48,15 +40,11 @@ function tagToDel() {
                     </div>
                   </div>
                 </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400" style="margin-bottom: 10px;">
-                  <button type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10" @click="toggleAdd">Ajouter</button> 
-                  <div v-if="showAdd">
-                    <input v-model="addTag" type="text" id="tagAdd" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Mot-clé à ajouter" @keyup.enter="tagToAdd"/>
-                  </div>
-                  <button type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10" @click="toggleDel">Supprimer</button>
-                  <div v-if="showDel">
-                    <input v-model="delTag" type="text" id="tagDel" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Mot-clé à supprimer" @keyup.enter="tagToDel"/>
-                  </div>
+                <p v-if="imgSup != null" class="text-base leading-relaxed text-gray-500" style="margin-bottom: 10px;">
+                  <b>Raisons de suppression:</b>
+                </p>
+                <p v-if="imgSup != null" class="text-base leading-relaxed text-gray-500" style="margin-bottom: 10px;">
+                  {{ imgSup }}
                 </p>
                 <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400" style="margin-bottom: 1px;">
                   <b>Détails :</b>
