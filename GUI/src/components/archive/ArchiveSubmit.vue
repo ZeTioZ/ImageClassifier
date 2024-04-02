2921ff2b933252d746b1e53b3b6a08dbc65ac235
Baptiste <baptiste.moncomble@student.unamur.be>
2024-04-02 10:47:08 +0200

    Batch image name (#23)
    * ajout de l'input du nom d'images
    
    * Update ArchiveSubmit.vue

--- GUI/src/components/archive/ArchiveSubmit.vue
+++ GUI/src/components/archive/ArchiveSubmit.vue
@@ -15,6 +15,17 @@ const addNewTag = () => {
   }
 };
 
+const newName = ref('');   // text in the input
+const NewName = ref('');   // valid new name
+
+// function to replace the NewName with the newName
+const addNewName = () => {
+  if (newName.value) {
+    NewName.value = newName.value;
+    newName.value = '';
+  }
+};
+
 // function to remove a tag from the newTags array with its given index
 const removeTag = (tagIndex) => {
   newTags.value.splice(tagIndex, 1);
@@ -28,7 +39,7 @@ const removeTag = (tagIndex) => {
       <!-- form -->
       <form v-on:submit.prevent="addNewTag">
         <div class="mb-2">
-          <label for="new-tag" class="italic text-sm text-white">Mots-clés par défaut</label>
+          <label for="new-tag" class="italic text-sm text-white">Mots-clés par défaut:</label>
         </div>
         <div class="flex flex-row w-full">
           <input v-model="newTag" id="new-tag" placeholder="Ex.: cérémonie, animateur, etc." autocomplete="off" class="bg-white text-gray-700 text-xs block p-1 w-full rounded-s focus:outline-none" />
@@ -37,11 +48,24 @@ const removeTag = (tagIndex) => {
       </form>
 
       <!-- added tags list -->
-      <ul class="text-sm space-y-1 text-white list-disc list-inside mt-2">
+      <ul class="overflow-y-auto max-h-24 text-sm space-y-1 text-white list-disc list-inside mt-2">
         <ArchiveSubmitNewTag v-for="tag, i in newTags" :tagName="tag" @remove="removeTag(i)" />
       </ul>
     </div>
 
+    <!-- Nom des images -->
+    <div>
+      <!-- form -->
+      <form v-on:submit.prevent="addNewName">
+        <div class="mb-2">
+          <label for="new-name" class="italic text-sm text-white">Nom des images par défaut: <span class="text-ls-vert-base">{{ NewName }}</span></label>
+        </div>
+        <div class="flex flex-row w-full">
+          <input v-model="newName" id="new-name" placeholder="Ex.: Camp-53e-2018" autocomplete="off" class="bg-white text-gray-700 text-xs block p-1 w-full rounded-s focus:outline-none" />
+          <button class="text-white bg-ls-vert-base p-1 w-10 rounded-e text-xl"><strong>✓</strong></button>
+        </div>
+      </form>
+    </div>
     <!-- submit button-->
     <div class="mt-2">
       <button class="text-ls-bleu-fonce bg-white hover:bg-ls-bleu-fonce hover:text-ls-vert-base border hover:border-ls-vert-base font-medium rounded-full text-xl px-5 py-2 focus:outline-none w-full transition duration-300" @click="$emit('submit')">Trier les images</button>
