eb6ef7ceca047eca69fa5fa1bd2d3fa94aff6e44
ZeTioZ <donato_gentile@hotmail.it>
2024-04-02 21:46:28 +0200

    Fixed drag on modal image.
    Took 2 hours 17 minutes

--- GUI/src/components/image/ModalImage.vue
+++ GUI/src/components/image/ModalImage.vue
@@ -1,52 +1,52 @@
 <script setup>
-import Tag from '@/components/image/Tag.vue';
-import Add from '@/components/icons/Add.vue';
-import {ref} from "vue";
-const add = ref(false)
-const props = defineProps(['imgSrc', 'imgName', 'imgTags', 'imgSize', 'imgSup']);
-const emit = defineEmits(['close', 'add', 'del']);
+  import Tag from '@/components/image/Tag.vue';
+  import Add from '@/components/icons/Add.vue';
+  import {ref} from "vue";
+  const add = ref(false)
+  const props = defineProps(['parent', 'imgSrc', 'imgName', 'imgTags', 'imgSize', 'imgSup']);
+  const emit = defineEmits(['close', 'add', 'del']);
 
-function closeModal() {
-  emit('close');
-}
-function tagToAdd() {
-  emit('add', addTag.value);
-}
-function tagToDel(term) {
-  emit('del', term.value);
-}
-function addTag(){
-  add.value = !add.value;
-}
-
+  function closeModal() {
+    emit('close');
+  }
+  function tagToAdd() {
+    emit('add', addTag.value);
+  }
+  function tagToDel(term) {
+    emit('del', term.value);
+  }
+  function addTag(){
+    add.value = !add.value;
+  }
 </script>
+
 <template>
 <!-- Extra Large Modal -->
-<div id="extralarge-modal" tabindex="-1" class="fixed inset-0 top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto bg-gray-600 bg-opacity-50 md:inset-0 h-[calc(100%-1rem)] max-h-full" @click.self="closeModal">
-    <div class="relative w-full max-w-7xl max-h-full">
+<div id="extralarge-modal" tabindex="-1" data-draggable="false" class="fixed inset-0 top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto bg-gray-600 bg-opacity-50 md:inset-0 h-screen" @click.self="closeModal">
+    <div class="relative">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow">
           <div class="modal-header">
-            <h2 class="text-xl font-bold">{{ imgName }}</h2>
+            <h2 class="text-xl font-bold pt-2">{{ imgName }}</h2>
           </div>
             <!-- Modal body -->
-            <div class="flex ">
+            <div class="flex">
               <div class="w-2/3">
-                <img :src="imgSrc" class="h-full w-full object-cover">
+                <img :src="imgSrc" alt="full" class="h-full w-full object-cover p-2">
               </div>
               <div class="w-1/3">
-                <p class="text-base leading-relaxed text-gray-500" style="margin-bottom: 1px;">
+                <p class="text-base leading-relaxed text-gray-500 pb-[1px]">
                   <b>Mots-clés :</b>
                 </p>
-                <p class="text-base leading-relaxed text-gray-500" style="margin-bottom: 10px;">
+                <p class="text-base leading-relaxed text-gray-500 pb-[10px]">
                   <div class="mt-0.5 overflow-y-auto scrollbar-hide">
                     <div class="flex m-3">
-                      <Tag v-for="tag in imgTags" @delete="tagToDel()" :key="tag.name" :tagName="tag.name" :class="tag.color" :inModal="'pomelo'"/>
                       <button @click="addTag" class="text-ls-bleu-fonce hover:bg-ls-bleu-fonce hover:text-white rounded text-sm p-1 transition duration-300">
                         <Add class="w-6 h-6" />
-                      </button>                      
+                      </button>
+                      <Tag v-for="tag in imgTags" @delete="tagToDel()" :key="tag.name" :tagName="tag.name" :class="tag.color" :inModal="'pomelo'"/>
                     </div>
-                    <input v-if="add" id="tag" placeholder="Mot-clé à rajouter" @keyup.enter="tagToAdd" autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-l-md block p-2 mx-2" />                   
+                    <input v-if="add" id="tag" placeholder="Mot-clé à rajouter" @keyup.enter="tagToAdd" autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-l-md block p-2 mx-2" />
                   </div>
                 </p>
                 <p v-if="imgSup != null" class="text-base leading-relaxed text-gray-500" style="margin-bottom: 10px;">
