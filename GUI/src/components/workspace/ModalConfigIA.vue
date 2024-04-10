9a064f3eae484b0c108e24e66f9fc71376f1153a
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-04-10 15:01:22 +0200

    Update ModalConfigIA.vue

--- GUI/src/components/workspace/ModalConfigIA.vue
+++ GUI/src/components/workspace/ModalConfigIA.vue
@@ -4,10 +4,29 @@ const netteté = ref(0);
 const length = ref(0);
 const hight = ref(0);
 const brigthness = ref(0);
+const Tag = ref('');
+const BlacklistedTags = ref([]);
+
 const emit = defineEmits(['close']);
 
 function closeModal() {
   emit('close');
+}
+
+function validateConfig() {
+  console.log('Netteté:', netteté.value);
+  console.log('Longueur:', length.value);
+  console.log('Hauteur:', hight.value);
+  console.log('Luminosité:', brigthness.value);
+  console.log('Tag blacklisté:', BlacklistedTags.value);
+  closeModal();
+}
+
+function BlacklistTags() {
+  if (Tag.value) {
+    BlacklistedTags.value.push(Tag.value);
+    Tag.value = '';
+  }
 }
 </script>
 
@@ -50,15 +69,15 @@ function closeModal() {
               <input v-model="brigthness" type="number" min="0" max="3000"/>
             </p>
             <p class="text-base leading-relaxed text-gray-500">
-              Mot-clé à blacklister :
+              Mot-clé à blacklister :{{ BlacklistedTags.join(', ')}}
               <input v-model="Tag" id="tag" placeholder="Ex.: voiture, piscine, etc." @keyup.enter="BlacklistTags"
               autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-l-md block p-1 w-full" />
             </p>
           </div>
           <!-- Modal footer -->
           <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Valider</button>
-            <button type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10" @click="closeModal">Annuler</button>
+            <button type="button" @click="validateConfig" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >Valider</button>
+            <button type="button" @click="closeModal" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10" >Annuler</button>
           </div>
         </div>
       </div>
