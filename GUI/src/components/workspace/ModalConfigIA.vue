f4158ebb4f459dbcd4ad5322ab80727871247e05
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-04-11 12:51:19 +0200

    Update ModalConfigIA.vue

--- GUI/src/components/workspace/ModalConfigIA.vue
+++ GUI/src/components/workspace/ModalConfigIA.vue
@@ -125,15 +125,14 @@ function closeModal() {
                 {{ tag }}
                 <button @click="remove(index)" class="bg-blue-700 text-white rounded border">
                   <XMark class="w-3 h-3" />
-                </button>    
-                ,           
+                </button>             
               </span>
               <input v-model="Tag" id="tag" placeholder="Ex.: voiture, piscine, etc." @keyup.enter="BlacklistTags"
-              autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-l-md block p-1 w-full" />
+              autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded block p-1 w-full" />
             </p>
           </div>
           <!-- Modal footer -->
-          <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+          <div class="flex justify-evenly items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
             <button type="button" @click="validateConfig" class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >Valider</button>
             <button type="button" @click="rebotConfig" class="text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >Défaut</button>
             <button type="button" @click="closeModal" class="text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 focus:z-10" >Annuler</button>
