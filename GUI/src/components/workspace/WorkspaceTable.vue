4aeac416acaabfbb118568b00bd2eef061d10d1e
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2024-04-22 23:22:54 +0200

    fix typo

--- GUI/src/components/workspace/WorkspaceTable.vue
+++ GUI/src/components/workspace/WorkspaceTable.vue
@@ -2,7 +2,7 @@
 import ImageCard from '@/components/image/ImageCard.vue';
 import draggable from 'vuedraggable';
 import { ref, watchEffect } from 'vue';
-import arow from '@/components/icons/Arow.vue';
+import arrows from '@/components/icons/Arrows.vue';
 
 const props = defineProps({
   workspaceName: String,
@@ -51,11 +51,11 @@ function onEnd(event) {
       <div>
         <!--si aucune image n'est séléctioné alors gris claire sans over et si une ou plusieurs image séléctionée gris foncé avec over-->
         <button v-if="props.selectedImages.length === 0" class=" px-1 py-1 bg-gray-300 text-white text-base font-medium rounded-md w-full shadow-sm ">
-          <arow class="w-6 h-6" />
+          <arrows class="w-6 h-6" />
         </button>
         <button v-else @click="moveImages(props.workspaceName)" class=" px-1 py-1 bg-gray-700 hover:bg-gray-800 focus:outline-none
           focus:ring-4 focus:ring-gray-300 text-white text-base font-medium rounded-md w-full shadow-sm ">
-          <arow class="w-6 h-6" />
+          <arrows class="w-6 h-6" />
         </button>
       </div>
     </div>
